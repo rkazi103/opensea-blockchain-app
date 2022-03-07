dfeddece2281f76ac1c7ef7c320aec864d976cbd
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-03-06 21:43:22 -0700

    chore: created sanity project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const ADMIN_REWRITE = {
+  source: "/admin/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/admin/:path*"
+      : "/admin/index.html",
+};
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  rewrites: () => [ADMIN_REWRITE],
+};
