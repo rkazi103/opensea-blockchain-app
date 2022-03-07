c61b94554aefa8cb3a6d4a157e044a26c3c87e5d
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-03-07 11:28:39 -0700

    feat: added sanity schemas

---
+++ admin/schemas/users.ts
@@ -0,0 +1,39 @@
+const users = {
+  name: "users",
+  title: "Users",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      title: "User Name",
+      type: "string",
+    },
+    {
+      name: "walletAddress",
+      title: "Wallet Address",
+      type: "string",
+    },
+    {
+      name: "profileImage",
+      title: "Profile Image",
+      type: "image",
+    },
+    {
+      name: "bannerImage",
+      title: "Banner Image",
+      type: "image",
+    },
+    {
+      name: "twitterHandle",
+      title: "Twitter Handle",
+      type: "string",
+    },
+    {
+      name: "igHandle",
+      title: "Instagram Handle",
+      type: "string",
+    },
+  ],
+};
+
+export default users;
