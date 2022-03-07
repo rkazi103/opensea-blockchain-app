8805b6c30af8ff9e04b6f33f837582f904bf8e80
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-03-07 12:36:02 -0700

    feat: added metamask auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { useWeb3 } from "@3rdweb/hooks";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
+  const { address, connectWallet } = useWeb3();
+
   return (
     <div className="h-screen w-screen overflow-x-hidden overflow-y-scroll">
       <Head>
@@ -11,8 +14,25 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-      <Hero />
+      {address ? (
+        <>
+          <Header />
+          <Hero />
+        </>
+      ) : (
+        <div className="flex h-screen w-screen flex-col items-center justify-center bg-[#3b3d42]">
+          <button
+            className="cursor-pointer rounded-lg border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold text-white"
+            onClick={() => connectWallet("injected")}
+          >
+            Connect Wallet
+          </button>
+          <div className="mt-4 text-center text-lg text-white">
+            You need Chrome to be
+            <br /> able to run this app.
+          </div>
+        </div>
+      )}
     </div>
   );
 };
