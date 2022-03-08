2f174cdefdc49f54c2a1356bf5827958d01aec4c
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-03-08 12:56:29 -0700

    feat: implemented selected nft logic

--- pages/nfts/[nftId].tsx
+++ pages/nfts/[nftId].tsx
@@ -1,7 +1,28 @@
+import { NFTMetadata } from "@3rdweb/sdk";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
+import { useListings } from "../../hooks/useListings";
+import { useNftModule } from "../../hooks/useNftModule";
 
 const Nft: NextPage = () => {
+  const [selectedNft, setSelectedNft] = useState<NFTMetadata>();
+  const router = useRouter();
+  const listings = useListings();
+  const nftModule = useNftModule();
+
+  useEffect(() => {
+    if (!nftModule) return;
+
+    (async () => {
+      const nfts = await nftModule.getAll();
+      const selectedNftItem = nfts.find(nft => nft.id === router.query.nftId);
+
+      setSelectedNft(selectedNftItem);
+    })();
+  }, [nftModule]);
+
   return (
     <div>
       <Header />
