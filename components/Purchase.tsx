9cb416a7340e37297bfecd9565af84f0ff6a08e7
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-03-08 19:55:16 -0700

    feat: finished purchase functionality

--- components/Purchase.tsx
+++ components/Purchase.tsx
@@ -6,6 +6,9 @@ import {
 } from "@3rdweb/sdk";
 import { NextComponentType, NextPageContext } from "next";
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { HiTag } from "react-icons/hi";
+import { IoMdWallet } from "react-icons/io";
 
 type PurchaseProps = {
   isListed: string;
@@ -40,7 +43,58 @@ const Purchase: NextComponentType<NextPageContext, {}, PurchaseProps> = ({
     setEnableButton(true);
   }, [selectedMarketNft, selectedNft]);
 
-  return <div>Purchase</div>;
+  const confirmPurchase = () =>
+    toast.success(`Purchase successful!`, {
+      style: {
+        background: "#04111d",
+        color: "#fff",
+      },
+    });
+
+  const buyItem = async (listingId: string, quantityDesired: number) => {
+    await marketplaceModule
+      ?.buyoutDirectListing({
+        listingId: listingId,
+        quantityDesired: quantityDesired,
+      })
+      .catch(error => console.error(error));
+
+    confirmPurchase();
+  };
+
+  return (
+    <div className="flex h-20 w-full items-center rounded-lg border border-[#151c22] bg-[#303339] px-12">
+      <Toaster position="top-center" reverseOrder={false} />
+
+      {isListed === "true" ? (
+        <>
+          <div
+            onClick={() => {
+              enableButton ? buyItem(selectedMarketNft?.id as string, 1) : null;
+            }}
+            className={`mr-8 flex cursor-pointer items-center rounded-lg bg-[#2081e2] py-2 px-12 hover:bg-[#42a0ff]`}
+          >
+            <IoMdWallet className="text-xl" />
+            <div className="ml-2 text-lg font-semibold">Buy Now</div>
+          </div>
+
+          <div
+            className={`mr-8 flex cursor-pointer items-center rounded-lg border border-[#151c22] bg-[#363840] py-2  px-12 hover:bg-[#4c505c]`}
+          >
+            <HiTag className="text-xl" />
+            <div className="ml-2 text-lg font-semibold">Make Offer</div>
+          </div>
+        </>
+      ) : (
+        <div
+          className={`mr-8 flex cursor-pointer items-center rounded-lg bg-[#2081e2] py-2 px-12 hover:bg-[#42a0ff]`}
+        >
+          <IoMdWallet className="text-xl" />
+          <div className="ml-2 text-lg font-semibold">List Item</div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Purchase;
